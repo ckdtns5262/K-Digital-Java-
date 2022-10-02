639c0931ef8937c584e66b21bb7161248b6c6be8
chang <ckdtns5262@nate.com>
2022-10-02 14:40:31 +0900

    자바중간시험 제출

--- edu/src/Pra/TestPersons.java
+++ edu/src/Pra/TestPersons.java
@@ -1,30 +1,41 @@
 package Pra;
 
-abstract class Person{
-	private String punm;
-	private String name;
-	private int age;
+abstract class Person {
+
+	String pnum;
+	String name;
+	int age;
 
 	public Person(String pnum, String name, int age) {
-		this.punm = pnum;
+		super();
+		this.pnum = pnum;
 		this.name = name;
 		this.age = age;
 	}
-	
-	abstract void show(); 
+
+	public String toString() {
+		return pnum + ":" + "이름 : " + "name" + "나이 : " + age;
+	}
+
+	abstract void show();
+
 	abstract void increase();
 
-	@Override
-	public String toString() {
-		return "Person [punm=" + punm + ", name=" + name + ", age=" + age + "]";
-	}
 }
 
-class Employee extends Person{
+class Employee extends Person {
 	private String job;
 	private String dept;
 	private int salary;
-	
+
+	public void show() {
+		System.out.println(super.toString() + ":" + job + ":" + dept + ":" + salary);
+	}
+
+	public void increase() {
+		this.salary += 0.1 * salary;
+	}
+
 	public Employee(String pnum, String name, int age, String job, String dept, int salary) {
 		super(pnum, name, age);
 		this.job = job;
@@ -32,119 +43,104 @@ class Employee extends Person{
 		this.salary = salary;
 	}
 	
-	@Override
-	public String toString() {
-		return super.toString()+"Employee [job=" + job + ", dept=" + dept + ", salary=" + salary + "]";
-	}
-
-	public void increase() {
-		this.salary += (this.salary) * 0.1;
-	}
-	public void show() {
-		System.out.println(this.toString());
-	} 
 }
-
-class Desinger extends Employee {
+	
+class Designer extends Employee {
 	private String language;
 	private int workYear;
+	private int salary;
+
 	
-	public Desinger(String pnum, String name, int age, String job, String dept, int salary, String language, int workYear) {
-		super(job, dept, salary, pnum, name, age);
+	public void show() {
+		System.out.println(super.toString() + ":" + language + ":" + workYear);
+	}
+	public void increase() {
+		this.salary += salary * 0.1;
+	}
+	
+	public Designer(String pnum, String name, int age, String job, String dept, int salary,String language, int workYear) {
+		super(pnum, name, age, job, dept, salary);
 		this.language = language;
 		this.workYear = workYear;
 	}
-
-	@Override
-	public String toString() {
-		return super.toString()+"Desinger [language=" + language + ", workYear=" + workYear + "]";
-	}
-	
-	
-	public void show() {
-		System.out.println(this.toString());
-	}
-
 }
+
 class Student extends Person {
 	private String schoolName;
 	private int schoolyear;
 	private int scholarship;
 	
-	public Student(String pnum, String name, int age, String schoolName, int schoolyear, int scholarship ) {
-		super(pnum, name, age);
+	public void show() {
+		System.out.println(super.toString()+":" +schoolName + ":" + schoolyear + ":" + scholarship);
+	}
+	
+	public void increase() {
+		this.scholarship += scholarship * 0.1;
+	}
+	
+	public Student(String pnum, String name, int age, String schoolName, int schoolyear, int scholarship) {
+		super(pnum,name, age );
 		this.schoolName = schoolName;
 		this.schoolyear = schoolyear;
 		this.scholarship = scholarship;
 	}
-
-	@Override
-	public String toString() {
-		return super.toString()+ "Student [schoolName=" + schoolName + ", schoolyear=" + schoolyear + ", scholarship=" + scholarship
-				+ "]";
-	}
 	
-	public void increase() {
-		this.scholarship += (this.scholarship)*0.1;
-	}
-	
-	public void show() {
-		System.out.println(this.toString());
-	}
 }
 
 class WorkStudent extends Student {
-	 private String job;
-	 private int dayPay;
-	 
-	 public WorkStudent(String pnum, String name, int age, String schoolName, int schoolyear, int scholarship, String job, int dayPay ) {
-		 super(pnum, name, age, schoolName, schoolyear, scholarship);
-		 this.job= job;
-		 this.dayPay =dayPay;
-	 }
-
-	@Override
-	public String toString() {
-		return super.toString()+"WorkStudent [job=" + job + ", dayPay=" + dayPay + "]";
-	}
-	 
+	private String job;
+	private int dayPay;
+	
 	public void show() {
-		System.out.println(this.toString());
+		System.out.println(super.toString() + ":" + job + ":" + dayPay);
+	}
+	public void increase() {
+		this.dayPay += dayPay * 0.1;
+	}
+
+	public WorkStudent(String pnum, String name, int age, String schoolName, int schoolYear, int scholarship,
+			String job, int dayPay) {
+			super(pnum, name, age, schoolName, schoolYear, scholarship);
+			this.job = job;
+			this.dayPay = dayPay;
 	}
 	
-	public void increase() {
-		this.dayPay +=  (this.dayPay) *0.1;
-	}
-	 
 }
 
 public class TestPersons {
 	static void showAll(Person[] p) {
-        for (int i=0; i<p.length; i++) {
-      	 p[i].show(); 
-      	 
-        }
-    }
-	static void increaseAll(Person[] p) {
-        for (int i=0; i<p.length; i++) {
-      	 p[i].increase(); 
-        }
-    }
-	static void getData(Person[] p) {
-		Employee e = new Employee("p01", "Hong", 10, "DB", "Design", 2000);
-		Desinger d = new Desinger("p02", "kim",20, "프론트개발자","Deesign2",3000,"자바스크립트",4);
-		Student s = new Student("p03", "Lee",30, "부산대",4, 5);
-		WorkStudent w = new WorkStudent("p04", "Yoon",40, "부경대", 3, 4,"개발자",4000);
-		
+		for(int i =0; i<p.length; i++) {
+			p[i].show();
+		}
+	}
+	static void increaseAll(Person[]p ) {
+		for(int i=0; i<p.length; i++) {
+			p[i].increase();
+		}
+	}
 
-		
+	static void getData(Person[]p) {
+		Employee e = new Employee("p01", "Hong", 10, "DB", "Design", 2000);
+		Designer d = new Designer("p02", "Lee", 20, "프론트", "Design2", 3000, "Java", 40);
+		Student s = new Student("p03", "Park", 30, "백엔드", 4000, 6);
+		WorkStudent w = new WorkStudent("p04", "Kim", 40, "부산대", 5, 6, "풀스택", 6000);
+		p[0]= e;
+		p[1]= d;
+		p[2]= s;
+		p[3]= w;
 	}
 	
 	public static void main(String[] args) {
-	
-		Person[] setPerson = new Person[1];
-		
-
+		Person[] setPerson = new Person[4];
+		getData(setPerson);
+		showAll(setPerson);
+		increaseAll(setPerson);
+		showAll(setPerson);
 	}
 
 }
+	
+	
+	
+
+
